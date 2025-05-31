4e96c268b3c292fe17632358d7a53936bd72e3d2
solidados <geekbrains.pk@internet.ru>
2025-05-31 21:57:09 +0400

    chore: create Dockerfile; update README and commands

---
+++ api/db.mjs
@@ -0,0 +1,65 @@
+export const db = [
+  {
+    title: "Pulp Fiction",
+    director: "Quentin Tarantino",
+    year: 1994,
+    genres: [ "crime", "drama" ],
+    rating: 8.9,
+    duration: {
+      hours: 2,
+      minutes: 34
+    },
+    reviews: [
+      {
+        name: "Jack",
+        text: "Amazing movie"
+      },
+      {
+        name: "Tom",
+        text: "Super cool"
+      }
+    ]
+  },
+  {
+    title: "Fight Club",
+    director: "David Fincher",
+    year: 1999,
+    genres: [ "drama" ],
+    rating: 8.8,
+    duration: {
+      hours: 2,
+      minutes: 19
+    },
+    reviews: [
+      {
+        name: "John",
+        text: "10 points"
+      },
+      {
+        name: "Jane",
+        text: "Very nice"
+      }
+    ]
+  },
+  {
+    title: "Forest Gump",
+    director: "Robert Zemeckis",
+    year: 1994,
+    genres: [ "drama" ],
+    rating: 9.2,
+    duration: {
+      hours: 2,
+      minutes: 27
+    },
+    reviews: [
+      {
+        name: "Pavel",
+        text: "I wish I'd run as Forest does"
+      },
+      {
+        name: "Erica",
+        text: "Nice movie"
+      }
+    ]
+  },
+]
---
+++ api/server.mjs
@@ -0,0 +1,17 @@
+import express from 'express'
+import cors from 'cors'
+
+import { db } from "./db.mjs";
+
+const PORT = 3000;
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.json(db)
+})
+
+app.listen(PORT, () => console.log(`Listening for requests on port: ${PORT}`))
